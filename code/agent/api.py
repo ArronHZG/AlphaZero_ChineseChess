2708c59c67eade9bfef02edca2651543e3de5ae4
houzhenggangGit <hou.zg@foxmail.com>
2018-04-29 21:11:20 +0800

    alphaZero算法实现 未测试

---
+++ code/agent/api.py
@@ -0,0 +1,79 @@
+from multiprocessing import connection, Pipe
+from threading import Thread
+
+import numpy as np
+
+from code.config import Config
+from code.utility.model_helper import load_best_model_weight, need_to_reload_best_model_weight
+from time import time
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+class CChessModelAPI:
+
+    def __init__(self, config: Config, agent_model):
+        self.agent_model = agent_model  # CChessModel
+        self.pipes = []  # use for communication between processes/threads
+        self.config = config
+        self.need_reload = True
+        self.done = False
+
+    def start(self):
+        prediction_worker = Thread(target=self.predict_batch_worker, name="prediction_worker")
+        prediction_worker.daemon = True
+        prediction_worker.start()
+
+    def get_pipe(self, need_reload=True):
+        me, you = Pipe()
+        self.pipes.append(me)
+        self.need_reload = need_reload
+        return you
+
+    def predict_batch_worker(self):
+        if self.config.internet.distributed:
+            self.try_reload_model_from_internet()
+        last_model_check_time = time()
+        while not self.done:
+            if last_model_check_time + 600 < time():
+                self.try_reload_model()
+                last_model_check_time = time()
+            ready = connection.wait(self.pipes, timeout=0.001)
+            if not ready:
+                continue
+            data, result_pipes, data_len = [], [], []
+            for pipe in ready:
+                while pipe.poll():
+                    try:
+                        tmp = pipe.recv()
+                        data.extend(tmp)
+                        data_len.append(len(tmp))
+                        result_pipes.append(pipe)
+                    except EOFError as e:
+                        logger.error(f"EOF error: {e}")
+                        pipe.close()
+            data = np.asarray(data, dtype=np.float32)
+            with self.agent_model.graph.as_default():
+                policy_ary, value_ary = self.agent_model.model.predict_on_batch(data)
+            buf = []
+            k, i = 0, 0
+            for p, v in zip(policy_ary, value_ary):
+                buf.append((p, float(v)))
+                k += 1
+                if k >= data_len[i]:
+                    result_pipes[i].send(buf)
+                    buf = []
+                    k = 0
+                    i += 1
+
+    def try_reload_model(self):
+        try:
+            if self.need_reload and need_to_reload_best_model_weight(self.agent_model):
+                with self.agent_model.graph.as_default():
+                    load_best_model_weight(self.agent_model)
+        except Exception as e:
+            logger.error(e)
+
+    def close(self):
+        self.done = True
---
+++ code/agent/mcts.py
@@ -0,0 +1,344 @@
+from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+from logging import getLogger
+from threading import Lock
+
+import numpy as np
+import code.environment.light.static_env as senv
+from code.config import Config
+from code.environment.light.lookup_tables import ActionLabelsRed
+from time import sleep
+
+logger = getLogger(__name__)
+
+
+class VisitState:
+    def __init__(self):
+        self.a = defaultdict(ActionState)  # key: action, value: ActionState
+        self.sum_n = 0  # visit count
+        self.visit = []  # thread id that has visited this state
+        self.p = None  # policy of this state
+        self.legal_moves = None  # all leagal moves of this state
+        self.waiting = False  # is waiting for NN's predict
+        self.w = 0
+
+
+class ActionState:
+    def __init__(self):
+        self.n = 0  # N(s, a) : visit count
+        self.w = 0  # W(s, a) : total action value
+        self.q = 0  # Q(s, a) = N / W : action value
+        self.p = 0  # P(s, a) : prior probability
+        self.next = None
+
+
+class CChessPlayer:
+    def __init__(self, config: Config, search_tree=None, pipes=None, play_config=None, enable_resign=False,
+                 debugging=False):
+        self.config = config
+        self.play_config = play_config or self.config.play
+        self.labels_n = len(ActionLabelsRed)
+        self.labels = ActionLabelsRed
+        self.move_lookup = {move: i for move, i in zip(self.labels, range(self.labels_n))}
+        self.pipe = pipes  # pipes that used to communicate with CChessModelAPI thread
+        self.node_lock = defaultdict(Lock)  # key: state key, value: Lock of that state
+
+        if search_tree is None:
+            self.tree = defaultdict(VisitState)  # key: state key, value: VisitState
+        else:
+            self.tree = search_tree
+        self.root_state = None
+        self.enable_resign = enable_resign
+        self.debugging = debugging
+        self.search_results = {}  # for debug
+        self.debug = {}
+        self.s_lock = Lock()
+        self.run_lock = Lock()
+        self.q_lock = Lock()  # queue lock
+        self.t_lock = Lock()
+        self.buffer_planes = []  # prediction queue
+        self.buffer_history = []
+        self.all_done = Lock()
+        self.num_task = 0
+        self.job_done = False
+        self.executor = ThreadPoolExecutor(max_workers=self.play_config.search_threads + 2)
+        self.executor.submit(self.receiver)
+        self.executor.submit(self.sender)
+
+    def close(self):
+        self.job_done = True
+        if self.executor is not None:
+            self.executor.shutdown()
+
+    def sender(self):
+        '''
+        send planes to neural network for prediction
+        '''
+        limit = 256  # max prediction queue size
+        while not self.job_done:
+            self.run_lock.acquire()
+            with self.q_lock:
+                l = min(limit, len(self.buffer_history))
+                if l > 0:
+                    t_data = self.buffer_planes[0:l]
+                    logger.debug(f"send queue size = {l}")
+                    self.pipe.send(t_data)
+                else:
+                    self.run_lock.release()
+                    sleep(0.001)
+        else:
+            self.close()
+
+    def receiver(self):
+        '''
+        receive policy and value from neural network
+        '''
+        while not self.job_done:
+            if self.pipe.poll(0.001):
+                rets = self.pipe.recv()
+            else:
+                continue
+            k = 0
+            with self.q_lock:
+                for ret in rets:
+                    logger.debug(f"NN ret, update tree buffer_history = {self.buffer_history}")
+                    self.executor.submit(self.update_tree, ret[0], ret[1], self.buffer_history[k])
+                    k = k + 1
+                self.buffer_planes = self.buffer_planes[k:]
+                self.buffer_history = self.buffer_history[k:]
+            self.run_lock.release()
+
+    def update_tree(self, p, v, history):
+        state = history.pop()
+        if p is not None:
+            with self.node_lock[state]:
+                logger.debug(f"return from NN state = {state}, v = {v}")
+                node = self.tree[state]
+                node.p = p
+                node.waiting = False
+                if self.debugging:
+                    self.debug[state] = (p, v)
+                for hist in node.visit:
+                    self.executor.submit(self.MCTS_search, state, hist)
+                node.visit = []
+
+        virtual_loss = self.config.play.virtual_loss
+        logger.debug(f"backup from {state}, v = {v}, history = {history}")
+        while len(history) > 0:
+            action = history.pop()
+            state = history.pop()
+            v = - v
+            with self.node_lock[state]:
+                node = self.tree[state]
+                action_state = node.a[action]
+                action_state.n += 1 - virtual_loss
+                action_state.w += v + virtual_loss
+                action_state.q = action_state.w * 1.0 / action_state.n
+                logger.debug(
+                    f"update value: state = {state}, action = {action}, n = {action_state.n}, w = {action_state.w}, q = {action_state.q}")
+
+        with self.t_lock:
+            self.num_task -= 1
+            logger.debug(f"finish 1, remain num task = {self.num_task}")
+            if self.num_task <= 0:
+                self.all_done.release()
+
+    def MCTS_search(self, state, history=[], is_root_node=False):
+        """
+        Monte Carlo Tree Search
+        """
+        while True:
+            logger.debug(f"start MCTS, state = {state}, history = {history}")
+            game_over, v, _ = senv.done(state)
+            if game_over:
+                self.executor.submit(self.update_tree, None, v, history)
+                break
+
+            with self.node_lock[state]:
+                if state not in self.tree:
+                    # Expand and Evaluate
+                    self.tree[state].sum_n = 1
+                    self.tree[state].legal_moves = senv.get_legal_moves(state)
+                    self.tree[state].waiting = True
+                    logger.debug(f"expand_and_evaluate {state}, sum_n = {self.tree[state].sum_n}, history = {history}")
+                    self.expand_and_evaluate(state, history)
+                    break
+
+                if state in history[:-1]:  # loop -> loss
+                    logger.debug(f"loop -> loss, state = {state}, history = {history[:-1]}")
+                    self.executor.submit(self.update_tree, None, 0, history)
+                    break
+
+                # Select
+                node = self.tree[state]
+                if node.waiting:
+                    node.visit.append(history)
+                    logger.debug(f"wait for prediction state = {state}")
+                    break
+                sel_action = self.select_action_q_and_u(state, is_root_node)
+                virtual_loss = self.config.play.virtual_loss
+                self.tree[state].sum_n += 1
+                logger.debug(f"node = {state}, sum_n = {node.sum_n}")
+                action_state = self.tree[state].a[sel_action]
+                action_state.n += virtual_loss
+                action_state.w -= virtual_loss
+                action_state.q = action_state.w / action_state.n
+                logger.debug(
+                    f"apply virtual_loss = {virtual_loss}, as.n = {action_state.n}, w = {action_state.w}, q = {action_state.q}")
+                if action_state.next is None:
+                    action_state.next = senv.step(state, sel_action)
+                logger.debug(f"step action {sel_action}, next = {action_state.next}")
+
+            history.append(sel_action)
+            state = action_state.next
+            history.append(state)
+
+    def action(self, state, turns, no_act=None):
+        self.all_done.acquire(True)
+        self.root_state = state
+        done = 0
+        if state in self.tree:
+            done = self.tree[state].sum_n
+        self.num_task = self.play_config.simulation_num_per_move - done
+
+        # MCTS search
+        if self.num_task > 0:
+            logger.debug(f"all_task = {self.num_task}")
+            all_tasks = self.num_task
+            batch = all_tasks // self.config.play.search_threads
+            if all_tasks % self.config.play.search_threads != 0:
+                batch += 1
+            for iter in range(batch):
+                self.num_task = min(self.config.play.search_threads, all_tasks - self.config.play.search_threads * iter)
+                logger.debug(f"iter = {iter}, num_task = {self.num_task}")
+                for i in range(self.num_task):
+                    self.executor.submit(self.MCTS_search, state, [state], True)
+                self.all_done.acquire(True)
+        self.all_done.release()
+        policy, resign = self.calc_policy(state, turns)
+        if resign:  # resign
+            return None, list(policy)
+        if no_act is not None:
+            for act in no_act:
+                policy[self.move_lookup[act]] = 0
+
+        my_action = int(np.random.choice(range(self.labels_n), p=self.apply_temperature(policy, turns)))
+        return self.labels[my_action], list(policy)
+
+    def select_action_q_and_u(self, state, is_root_node):
+        '''
+        Select an action with highest Q(s,a) + U(s,a)
+        '''
+        is_root_node = self.root_state == state
+        logger.debug(f"select_action_q_and_u for {state}, root = {is_root_node}")
+        node = self.tree[state]
+        legal_moves = node.legal_moves
+
+        # push p, the prior probability to the edge (node.p), only consider legal moves
+        if node.p is not None:
+            all_p = 0
+            for mov in legal_moves:
+                mov_p = node.p[self.move_lookup[mov]]
+                node.a[mov].p = mov_p
+                all_p += mov_p
+            # rearrange the distribution
+            if all_p == 0:
+                all_p = 1
+            for mov in legal_moves:
+                node.a[mov].p /= all_p
+            # release the temp policy
+            node.p = None
+
+        # sqrt of sum(N(s, b); for all b)
+        xx_ = np.sqrt(node.sum_n + 1)
+
+        e = self.play_config.noise_eps
+        c_puct = self.play_config.c_puct
+        dir_alpha = self.play_config.dirichlet_alpha
+
+        best_score = -99999999
+        best_action = None
+
+        for mov in legal_moves:
+            action_state = node.a[mov]
+            p_ = action_state.p
+            if is_root_node:
+                p_ = (1 - e) * p_ + e * np.random.dirichlet([dir_alpha])[0]
+            # Q + U
+            score = action_state.q + c_puct * p_ * xx_ / (1 + action_state.n)
+            if score > 0.1 and is_root_node:
+                logger.debug(f"U+Q = {score:.2f}, move = {mov}, q = {action_state.q:.2f}")
+            if action_state.q > (1 - 1e-7):
+                best_action = mov
+                break
+            if score > best_score:
+                best_score = score
+                best_action = mov
+
+        if best_action == None:
+            logger.error(f"Best action is None, legal_moves = {legal_moves}, best_score = {best_score}")
+        if is_root_node:
+            logger.debug(f"selected action = {best_action}, with U + Q = {best_score}")
+        return best_action
+
+    def expand_and_evaluate(self, state, history):
+        '''
+        Evaluate the state, return its policy and value computed by neural network
+        '''
+        state_planes = senv.state_to_planes(state)
+        with self.q_lock:
+            self.buffer_planes.append(state_planes)
+            self.buffer_history.append(history)
+            logger.debug(f"EAE append buffer_history history = {history}")
+
+    def calc_policy(self, state, turns):
+        '''
+        calculate π(a|s0) according to the visit count
+        '''
+        node = self.tree[state]
+        policy = np.zeros(self.labels_n)
+        max_q_value = -100
+        debug_result = {}
+
+        for mov, action_state in node.a.items():
+            policy[self.move_lookup[mov]] = action_state.n
+            if self.debugging:
+                debug_result[mov] = (action_state.n, action_state.q, action_state.p)
+            if action_state.q > max_q_value:
+                max_q_value = action_state.q
+
+        if max_q_value < self.play_config.resign_threshold and self.enable_resign and turns > self.play_config.min_resign_turn:
+            return policy, True
+
+        if self.debugging:
+            temp = sorted(range(len(policy)), key=lambda k: policy[k], reverse=True)
+            for i in range(5):
+                index = temp[i]
+                mov = ActionLabelsRed[index]
+                if mov in debug_result:
+                    self.search_results[mov] = debug_result[mov]
+
+        policy /= np.sum(policy)
+        return policy, False
+
+    def apply_temperature(self, policy, turn):
+        if turn < 30 and self.play_config.tau_decay_rate != 0:
+            tau = tau = np.power(self.play_config.tau_decay_rate, turn + 1)
+        else:
+            tau = 0
+        if tau < 0.1:
+            tau = 0
+        if tau == 0:
+            action = np.argmax(policy)
+            ret = np.zeros(self.labels_n)
+            ret[action] = 1.0
+            return ret
+        else:
+            ret = np.power(policy, 1 / tau)
+            ret /= np.sum(ret)
+            return ret
+
+
+if __name__=='__main__':
+    c=CChessPlayer()
+
