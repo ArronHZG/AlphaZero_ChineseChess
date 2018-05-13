e1e03f73788c9b81e00ab17d6453935ccaada956
houzhenggangGit <hou.zg@foxmail.com>
2018-05-13 16:23:06 +0800

    增加worker

--- src/agent/mcts.py
+++ src/agent/mcts.py
@@ -3,14 +3,14 @@ from concurrent.futures import ThreadPoolExecutor
 from logging import getLogger
 from threading import Lock
 import concurrent.futures.thread
-import gc
-import sys
 
 import numpy as np
 import src.environment.light.static_env as senv
 from src.config import Config
 from src.environment.light.lookup_tables import ActionLabelsRed, flip_move
 from time import time, sleep
+import gc
+import sys
 
 logger = getLogger(__name__)
 
@@ -69,7 +69,10 @@ class CChessPlayer:
         self.num_task = 0
         self.done_tasks = 0
         self.uci = uci
+        self.no_act = None
+
         self.job_done = False
+
         self.executor = ThreadPoolExecutor(max_workers=self.play_config.search_threads + 2)
         self.executor.submit(self.receiver)
         self.executor.submit(self.sender)
@@ -88,7 +91,7 @@ class CChessPlayer:
             # self.executor = None
             self.executor._threads.clear()
             concurrent.futures.thread._threads_queues.clear()
-        policy, resign = self.calc_policy(state, turns)
+        policy, resign = self.calc_policy(state, turns, no_act)
         if resign:  # resign
             return None
         if no_act is not None:
@@ -112,7 +115,7 @@ class CChessPlayer:
                 l = min(limit, len(self.buffer_history))
                 if l > 0:
                     t_data = self.buffer_planes[0:l]
-                    logger.debug(f"send queue size = {l}")
+                    # logger.debug(f"send queue size = {l}")
                     self.pipe.send(t_data)
                 else:
                     self.run_lock.release()
@@ -130,7 +133,7 @@ class CChessPlayer:
             k = 0
             with self.q_lock:
                 for ret in rets:
-                    logger.debug(f"NN ret, update tree buffer_history = {self.buffer_history}")
+                    # logger.debug(f"NN ret, update tree buffer_history = {self.buffer_history}")
                     self.executor.submit(self.update_tree, ret[0], ret[1], self.buffer_history[k])
                     # self.update_tree(ret[0], ret[1], self.buffer_history[k])
                     k = k + 1
@@ -141,9 +144,12 @@ class CChessPlayer:
     def action(self, state, turns, no_act=None, depth=None, infinite=False):
         self.all_done.acquire(True)
         self.root_state = state
+        self.no_act = no_act
         done = 0
         if state in self.tree:
             done = self.tree[state].sum_n
+        if no_act:
+            done = 0
         self.done_tasks = done
         self.num_task = self.play_config.simulation_num_per_move - done
         if depth:
@@ -158,11 +164,11 @@ class CChessPlayer:
             batch = all_tasks // self.config.play.search_threads
             if all_tasks % self.config.play.search_threads != 0:
                 batch += 1
-            logger.debug(f"all_task = {self.num_task}, batch = {batch}")
+            # logger.debug(f"all_task = {self.num_task}, batch = {batch}")
             for iter in range(batch):
                 self.num_task = min(self.config.play.search_threads, all_tasks - self.config.play.search_threads * iter)
                 self.done_tasks += self.num_task
-                logger.debug(f"iter = {iter}, num_task = {self.num_task}")
+                # logger.debug(f"iter = {iter}, num_task = {self.num_task}")
                 for i in range(self.num_task):
                     self.executor.submit(self.MCTS_search, state, [state], True)
                 self.all_done.acquire(True)
@@ -173,7 +179,7 @@ class CChessPlayer:
                     self.print_depth_info(state, turns, start_time, value, no_act)
         self.all_done.release()
 
-        policy, resign = self.calc_policy(state, turns)
+        policy, resign = self.calc_policy(state, turns, no_act)
 
         if resign:  # resign
             return None, list(policy)
@@ -189,7 +195,7 @@ class CChessPlayer:
         Monte Carlo Tree Search
         """
         while True:
-            logger.debug(f"start MCTS, state = {state}, history = {history}")
+            # logger.debug(f"start MCTS, state = {state}, history = {history}")
             game_over, v, _ = senv.done(state)
             if game_over:
                 self.executor.submit(self.update_tree, None, v, history)
@@ -201,12 +207,12 @@ class CChessPlayer:
                     self.tree[state].sum_n = 1
                     self.tree[state].legal_moves = senv.get_legal_moves(state)
                     self.tree[state].waiting = True
-                    logger.debug(f"expand_and_evaluate {state}, sum_n = {self.tree[state].sum_n}, history = {history}")
+                    # logger.debug(f"expand_and_evaluate {state}, sum_n = {self.tree[state].sum_n}, history = {history}")
                     self.expand_and_evaluate(state, history)
                     break
 
                 if state in history[:-1]:  # loop -> loss
-                    logger.debug(f"loop -> loss, state = {state}, history = {history[:-1]}")
+                    # logger.debug(f"loop -> loss, state = {state}, history = {history[:-1]}")
                     self.executor.submit(self.update_tree, None, 0, history)
                     break
 
@@ -214,39 +220,38 @@ class CChessPlayer:
                 node = self.tree[state]
                 if node.waiting:
                     node.visit.append(history)
-                    logger.debug(f"wait for prediction state = {state}")
+                    # logger.debug(f"wait for prediction state = {state}")
                     break
 
                 sel_action = self.select_action_q_and_u(state, is_root_node)
 
                 virtual_loss = self.config.play.virtual_loss
                 self.tree[state].sum_n += 1
-                logger.debug(f"node = {state}, sum_n = {node.sum_n}")
+                # logger.debug(f"node = {state}, sum_n = {node.sum_n}")
 
                 action_state = self.tree[state].a[sel_action]
                 action_state.n += virtual_loss
                 action_state.w -= virtual_loss
                 action_state.q = action_state.w / action_state.n
 
-                logger.debug(
-                    f"apply virtual_loss = {virtual_loss}, as.n = {action_state.n}, w = {action_state.w}, q = {action_state.q}")
+                # logger.debug(f"apply virtual_loss = {virtual_loss}, as.n = {action_state.n}, w = {action_state.w}, q = {action_state.q}")
 
                 # if action_state.next is None:
                 history.append(sel_action)
                 state = senv.step(state, sel_action)
                 history.append(state)
-                logger.debug(f"step action {sel_action}, next = {action_state.next}")
+                # logger.debug(f"step action {sel_action}, next = {action_state.next}")
 
             # history.append(sel_action)
             # state = action_state.next
             # history.append(state)
 
-    def select_action_q_and_u(self, state, is_root_node):
+    def select_action_q_and_u(self, state):
         '''
         Select an action with highest Q(s,a) + U(s,a)
         '''
         is_root_node = self.root_state == state
-        logger.debug(f"select_action_q_and_u for {state}, root = {is_root_node}")
+        # logger.debug(f"select_action_q_and_u for {state}, root = {is_root_node}")
         node = self.tree[state]
         legal_moves = node.legal_moves
 
@@ -276,6 +281,9 @@ class CChessPlayer:
         best_action = None
 
         for mov in legal_moves:
+            if is_root_node and self.no_act and mov in self.no_act:
+                # logger.debug(f"mov = {mov}, no_act = {self.no_act}, continue")
+                continue
             action_state = node.a[mov]
             p_ = action_state.p
             if is_root_node:
@@ -305,7 +313,7 @@ class CChessPlayer:
         with self.q_lock:
             self.buffer_planes.append(state_planes)
             self.buffer_history.append(history)
-            logger.debug(f"EAE append buffer_history history = {history}")
+            # logger.debug(f"EAE append buffer_history history = {history}")
 
     def update_tree(self, p, v, history):
         state = history.pop()
@@ -313,7 +321,7 @@ class CChessPlayer:
 
         if p is not None:
             with self.node_lock[state]:
-                logger.debug(f"return from NN state = {state}, v = {v}")
+                # logger.debug(f"return from NN state = {state}, v = {v}")
                 node = self.tree[state]
                 node.p = p
                 node.waiting = False
@@ -324,7 +332,7 @@ class CChessPlayer:
                 node.visit = []
 
         virtual_loss = self.config.play.virtual_loss
-        logger.debug(f"backup from {state}, v = {v}, history = {history}")
+        # logger.debug(f"backup from {state}, v = {v}, history = {history}")
         while len(history) > 0:
             action = history.pop()
             state = history.pop()
@@ -335,16 +343,15 @@ class CChessPlayer:
                 action_state.n += 1 - virtual_loss
                 action_state.w += v + virtual_loss
                 action_state.q = action_state.w * 1.0 / action_state.n
-                logger.debug(
-                    f"update value: state = {state}, action = {action}, n = {action_state.n}, w = {action_state.w}, q = {action_state.q}")
+                # logger.debug(f"update value: state = {state}, action = {action}, n = {action_state.n}, w = {action_state.w}, q = {action_state.q}")
 
         with self.t_lock:
             self.num_task -= 1
-            logger.debug(f"finish 1, remain num task = {self.num_task}")
+            # logger.debug(f"finish 1, remain num task = {self.num_task}")
             if self.num_task <= 0:
                 self.all_done.release()
 
-    def calc_policy(self, state, turns):
+    def calc_policy(self, state, turns, no_act):
         '''
         calculate π(a|s0) according to the visit count
         '''
@@ -355,6 +362,9 @@ class CChessPlayer:
 
         for mov, action_state in node.a.items():
             policy[self.move_lookup[mov]] = action_state.n
+            if no_act and mov in no_act:
+                policy[self.move_lookup[mov]] = 0
+                continue
             if self.debugging:
                 debug_result[mov] = (action_state.n, action_state.q, action_state.p)
             if action_state.q > max_q_value:
@@ -410,12 +420,12 @@ class CChessPlayer:
         logger.debug(output)
         sys.stdout.flush()
 
-    def apply_temperature(self, policy, turn):
+    def apply_temperature(self, policy, turn) -> np.ndarray:
         if turn < 30 and self.play_config.tau_decay_rate != 0:
-            tau = tau = np.power(self.play_config.tau_decay_rate, turn + 1)
+            tau = np.power(self.play_config.tau_decay_rate, turn + 1)
         else:
             tau = 0
-        if tau < 0.1:
+        if tau < 0.1 or (turn >= 4 and self.config.opts.evaluate):
             tau = 0
         if tau == 0:
             action = np.argmax(policy)
---
+++ src/worker/optimize.py
@@ -0,0 +1,267 @@
+import os
+import subprocess
+import shutil
+import numpy as np
+
+from collections import deque
+from concurrent.futures import ProcessPoolExecutor
+from logging import getLogger
+from random import shuffle
+import src.environment.light.static_env as senv
+from src.environment.light.lookup_tables import Winner, ActionLabelsRed, flip_policy, flip_move
+from src.agent.model import CChessModel
+from src.config import Config
+from src.utils.data_helper import get_game_data_filenames, read_game_data_from_file
+from src.utils.model_helper import load_best_model_weight, save_as_best_model
+from src.utils.model_helper import need_to_reload_best_model_weight, save_as_next_generation_model, save_as_best_model
+from src.utils.tf_util import set_session_config
+from keras.optimizers import SGD
+from keras.callbacks import TensorBoard
+import keras.backend as K
+from keras.utils import multi_gpu_model
+logger = getLogger(__name__)
+
+def start(config: Config):
+    set_session_config(per_process_gpu_memory_fraction=1, allow_growth=True, device_list=config.opts.device_list)
+    return OptimizeWorker(config).start()
+
+class OptimizeWorker:
+    def __init__(self, config:Config):
+        self.config = config
+        self.model = None
+        self.loaded_filenames = set()
+        self.loaded_data = deque(maxlen=self.config.trainer.dataset_size)
+        self.dataset = deque(), deque(), deque()
+        self.executor = ProcessPoolExecutor(max_workers=config.trainer.cleaning_processes)
+        self.filenames = []
+        self.opt = None
+        self.count = 0
+        self.eva = False
+
+    def start(self):
+        self.model = self.load_model()
+        self.training()
+
+    def training(self):
+        self.compile_model()
+        total_steps = self.config.trainer.start_total_steps
+        bef_files = []
+        last_file = None
+
+        while True:
+            files = get_game_data_filenames(self.config.resource)
+            offset = self.config.trainer.min_games_to_begin_learn
+            if (len(files) < self.config.trainer.min_games_to_begin_learn \
+              or ((last_file is not None) and files.index(last_file) + 1 + offset > len(files))):
+                # if last_file is not None:
+                #     logger.info('Waiting for enough data 300s, ' + str((len(files) - files.index(last_file)) * self.config.play_data.nb_game_in_file) \
+                #             +' vs '+ str(self.config.trainer.min_games_to_begin_learn)+' games')
+                # else:
+                #     logger.info('Waiting for enough data 300s, ' + str(len(files) * self.config.play_data.nb_game_in_file) \
+                #             +' vs '+ str(self.config.trainer.min_games_to_begin_learn)+' games')
+                # time.sleep(300)
+                if last_file is not None:
+                    self.save_current_model(send=True)
+                break
+            else:
+                if last_file is not None:
+                    idx = files.index(last_file) + 1
+                    if len(files) - idx > self.config.trainer.load_step:
+                        files = files[idx:idx + self.config.trainer.load_step]
+                    else:
+                        files = files[idx:]
+                elif len(files) > self.config.trainer.load_step:
+                    files = files[0:self.config.trainer.load_step]
+                last_file = files[-1]
+                logger.info(f"Last file = {last_file}")
+                self.filenames = deque(files)
+                logger.debug(f"Start training {len(self.filenames)} files")
+                shuffle(self.filenames)
+                self.fill_queue()
+                if len(self.dataset[0]) > self.config.trainer.batch_size:
+                    steps = self.train_epoch(self.config.trainer.epoch_to_checkpoint)
+                    total_steps += steps
+                    self.save_current_model(send=False)
+                    self.update_learning_rate(total_steps)
+                    self.count += 1
+                    a, b, c = self.dataset
+                    a.clear()
+                    b.clear()
+                    c.clear()
+                    # self.remove_play_data()
+
+    def train_epoch(self, epochs):
+        tc = self.config.trainer
+        state_ary, policy_ary, value_ary = self.collect_all_loaded_data()
+        tensorboard_cb = TensorBoard(log_dir="./logs", batch_size=tc.batch_size, histogram_freq=1)
+        if self.config.opts.use_multiple_gpus:
+            self.mg_model.fit(state_ary, [policy_ary, value_ary],
+                                 batch_size=tc.batch_size,
+                                 epochs=epochs,
+                                 shuffle=True,
+                                 validation_split=0.02,
+                                 callbacks=[tensorboard_cb])
+        else:
+            self.model.model.fit(state_ary, [policy_ary, value_ary],
+                                 batch_size=tc.batch_size,
+                                 epochs=epochs,
+                                 shuffle=True,
+                                 validation_split=0.02,
+                                 callbacks=[tensorboard_cb])
+        steps = (state_ary.shape[0] // tc.batch_size) * epochs
+        return steps
+
+    def compile_model(self):
+        self.opt = SGD(lr=0.003, momentum=self.config.trainer.momentum)
+        losses = ['categorical_crossentropy', 'mean_squared_error']
+        if self.config.opts.use_multiple_gpus:
+            self.mg_model = multi_gpu_model(self.model.model, gpus=self.config.opts.gpu_num)
+            self.mg_model.compile(optimizer=self.opt, loss=losses, loss_weights=self.config.trainer.loss_weights)
+        else:
+            self.model.model.compile(optimizer=self.opt, loss=losses, loss_weights=self.config.trainer.loss_weights)
+
+    def update_learning_rate(self, total_steps):
+        # The deepmind paper says
+        # ~400k: 1e-2
+        # 400k~600k: 1e-3
+        # 600k~: 1e-4
+
+        lr = self.decide_learning_rate(total_steps)
+        if lr:
+            K.set_value(self.opt.lr, lr)
+            logger.debug(f"total step={total_steps}, set learning rate to {lr}")
+
+    def fill_queue(self):
+        futures = deque()
+        n = len(self.filenames)
+        with ProcessPoolExecutor(max_workers=self.config.trainer.cleaning_processes) as executor:
+            for _ in range(self.config.trainer.cleaning_processes):
+                if len(self.filenames) == 0:
+                    break
+                filename = self.filenames.pop()
+                # logger.debug("loading data from %s" % (filename))
+                futures.append(executor.submit(load_data_from_file, filename))
+            while futures and len(self.dataset[0]) < self.config.trainer.dataset_size: #fill tuples
+                _tuple = futures.popleft().result()
+                if _tuple is not None:
+                    for x, y in zip(self.dataset, _tuple):
+                        x.extend(y)
+                m = len(self.filenames)
+                if m > 0:
+                    if (n - m) % 1000 == 0:
+                        logger.info(f"Reading {n - m} files")
+                    filename = self.filenames.pop()
+                    # logger.debug("loading data from %s" % (filename))
+                    futures.append(executor.submit(load_data_from_file, filename))
+
+    def collect_all_loaded_data(self):
+        state_ary, policy_ary, value_ary = self.dataset
+
+        state_ary1 = np.asarray(state_ary, dtype=np.float32)
+        policy_ary1 = np.asarray(policy_ary, dtype=np.float32)
+        value_ary1 = np.asarray(value_ary, dtype=np.float32)
+        return state_ary1, policy_ary1, value_ary1
+
+    def load_model(self):
+        model = CChessModel(self.config)
+        if self.config.opts.new or not load_best_model_weight(model):
+            model.build()
+            save_as_best_model(model)
+        return model
+
+    def save_current_model(self, send=False):
+
+        if not send:
+            # save_as_best_model(self.model)
+            pass
+        else:
+            save_as_next_generation_model(self.model)
+            logger.info("Save as ng model")
+
+
+    def decide_learning_rate(self, total_steps):
+        ret = None
+
+        for step, lr in self.config.trainer.lr_schedules:
+            if total_steps >= step:
+                ret = lr
+        return ret
+
+    def try_reload_model(self):
+        logger.debug("check model")
+        if need_to_reload_best_model_weight(self.model):
+            with self.model.graph.as_default():
+                load_best_model_weight(self.model)
+            return True
+        return False
+
+    def remove_play_data(self):
+        files = get_game_data_filenames(self.config.resource)
+        backup_folder = os.path.join(self.config.resource.data_dir, 'backup');
+        try:
+            for i in range(len(files)):
+                # os.remove(files[i])
+                shutil.move(files[i], backup_folder)
+        except:
+            pass
+
+def load_data_from_file(filename):
+    try:
+        data = read_game_data_from_file(filename)
+    except Exception as e:
+        logger.error(f"Error when loading data {e}")
+        return None
+    if data is None:
+        return None
+    return expanding_data(data)
+
+def expanding_data(data):
+    state = data[0]
+    real_data = []
+    action = None
+    policy = None
+    value = None
+    for item in data[1:]:
+        action = item[0]
+        value = item[1]
+        try:
+            policy = build_policy(action, flip=False)
+        except Exception as e:
+            logger.error(f"Expand data error {e}, item = {item}, data = {data}, state = {state}")
+            return None
+        real_data.append([state, policy, value])
+        state = senv.step(state, action)
+        
+    return convert_to_trainging_data(real_data)
+
+
+def convert_to_trainging_data(data):
+    state_list = []
+    policy_list = []
+    value_list = []
+
+    for state, policy, value in data:
+        state_planes = senv.state_to_planes(state)
+        sl_value = value
+
+        state_list.append(state_planes)
+        policy_list.append(policy)
+        value_list.append(sl_value)
+
+    return np.asarray(state_list, dtype=np.float32), \
+           np.asarray(policy_list, dtype=np.float32), \
+           np.asarray(value_list, dtype=np.float32)
+
+def build_policy(action, flip):
+    labels_n = len(ActionLabelsRed)
+    move_lookup = {move: i for move, i in zip(ActionLabelsRed, range(labels_n))}
+    policy = np.zeros(labels_n)
+
+    policy[move_lookup[action]] = 1
+
+    if flip:
+        policy = flip_policy(policy)
+    return list(policy)
+
+
+
